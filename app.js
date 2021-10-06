81564cd8217d7d5c00464e65265b9179bdad0d18
mcysneiros <msocys@gmail.com>
2021-10-06 20:40:18 -0300

    eac

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
-storeBtn.addEventListener('click', () => {
-  const userId = 'u123';
-  document.cookie = `uid=${userId}`;
-});
+indexedDB.open('StorageDummy', 1);
+
+storeBtn.addEventListener('click', () => {});
 retrBtn.addEventListener('click', () => {});
