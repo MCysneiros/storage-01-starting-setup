2ba046cf782031bb90c37b2f4a1cf6cdee4fb6f9
mcysneiros <msocys@gmail.com>
2021-10-07 17:43:43 -0300

    11111

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
-indexedDB.open('StorageDummy', 1);
+const dbRequest = indexedDB.open('StorageDummy', 1);
 
-storeBtn.addEventListener('click', () => {});
-retrBtn.addEventListener('click', () => {});
+let db;
+
+dbRequest.onsuccess = function (event) {
+  db = event.target.result;
+};
+dbRequest.onupgradeneeded = function (event) {
+  db = event.target.result;
+  const objStore = db.createObjectStore('products', { keyPath: 'id' });
+
+  objStore.transaction.oncomplete = function (event) {
+    const productsStore = db
+      .transaction('products', 'readwrite')
+      .objectStore('products');
+    productsStore.add({
+      id: 'p1',
+      title: 'the first product',
+      price: 1299,
+      tags: ['luxury', 'expensive'],
+    });
+  };
+};
+dbRequest.onerror = function (event) {
+  console.log('error');
+};
+
+storeBtn.addEventListener('click', () => {
+  if (!db) {
+    return;
+  }
+
+  const productsStore = db
+    .transaction('products', 'readwrite')
+    .objectStore('products');
+  productsStore.add({
+    id: 'p2',
+    title: 'the second product',
+    price: 1239,
+    tags: ['luxury', 'expensive'],
+  });
+});
+retrBtn.addEventListener('click', () => {
+  const productsStore = db
+    .transaction('products', 'readwrite')
+    .objectStore('products');
+  const request = productsStore.get('p2');
+
+  request.onsuccess = () => {
+    console.log(request.result);
+  };
+});
